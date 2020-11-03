fc7e203905b64580d51c043e6673042b426248e0
Rin Kishi <rinetranger@gmail.com>
2020-11-03 22:55:29 +0900

    onclick&onchange

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React,{useState} from 'react';
 
-const App=()=> {
-  const [count,setCount] = useState(0)
-  const increment =()=>setCount(count+1)
-  const decrement =()=>setCount(count-1)
-  const increment2 =()=>setCount(previousCount=>previousCount+1)
-  const decrement2 =()=>setCount(previousCount=>previousCount-1)
-  const reset = ()=>setCount(0)
-  const double =()=>setCount(count*2)
-  const dividezero =()=>{
-    if(count%3==0){
-      setCount(count/3)
-    }else{
-      setCount(count)
-    }
+const App=(props)=> {
+  const initialStates ={
+    name:'',
+    price:1000
   }
+const [name,setName]=useState(initialStates.name)
+const [price,setPrice]=useState(initialStates.price)
+const reset=()=>{
+  setPrice(props.price)
+  setName(props.name)
+}
   return (
     <div>
-      <div>
-        count:{count}
-      </div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={increment2}>+1</button>
-      <button onClick={decrement2}>-1</button>
-      <button onClick={double}>X2</button>
-      <button onClick={reset}>reset</button>
-      <button onClick={dividezero}>3の倍数で割り切れる場合の商</button>
+     <p>現在の{name}は{price}です。</p>
+     <button onClick={()=>{setPrice(price+100)}}>+100</button>
+     <button onClick={()=>{setPrice(price-100)}}>-100</button>
+     <button onClick={reset}>reset</button>
+     <input value={name} onChange={e =>setName(e.target.value)} />
     </div>
   );
+}
+App.defaultProps={
+  name:'',
+  price:1000
 }
 
 export default App;
