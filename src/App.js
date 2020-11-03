6074b10dce149268d6239b8de58a862e50eadd1a
Rin Kishi <rinetranger@gmail.com>
2020-11-03 23:29:33 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 
 const App=(props)=> {
 const [state,setState]=useState(props)
 const {name,price}=state
+useEffect(()=>{
+  console.log('useEffect');
+})
+useEffect(()=>{
+  console.log('useEffect2');
+},[])
+useEffect(()=>{
+  console.log('useEffectfor name');
+},[name])
+
   return (
     <div>
-     <p>現在の{state.name}は{state.price}です。</p>
+     <p>現在の{state.name}は{state.price}です.</p>
      <button onClick={()=>{setState({...state,price:state.price+100})}}>+100</button>
      <button onClick={()=>{setState({...state,price:state.price-100})}}>-100</button>
      <button onClick={()=>{setState(props)}}>reset</button>
