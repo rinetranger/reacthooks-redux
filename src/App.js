329f077e600f56ce3e01a8fc7d5ad683fba3eb75
Rin Kishi <rinetranger@gmail.com>
2020-11-03 23:09:31 +0900

    refactoringusestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React,{useState} from 'react';
 
 const App=(props)=> {
-  const initialStates ={
-    name:'',
-    price:1000
-  }
-const [name,setName]=useState(initialStates.name)
-const [price,setPrice]=useState(initialStates.price)
-const reset=()=>{
-  setPrice(props.price)
-  setName(props.name)
-}
+const [state,setState]=useState(props)
+const {name,price}=state
   return (
     <div>
-     <p>現在の{name}は{price}です。</p>
-     <button onClick={()=>{setPrice(price+100)}}>+100</button>
-     <button onClick={()=>{setPrice(price-100)}}>-100</button>
-     <button onClick={reset}>reset</button>
-     <input value={name} onChange={e =>setName(e.target.value)} />
+     <p>現在の{state.name}は{state.price}です。</p>
+     <button onClick={()=>{setState({...state,price:state.price+100})}}>+100</button>
+     <button onClick={()=>{setState({...state,price:state.price-100})}}>-100</button>
+     <button onClick={()=>{setState(props)}}>reset</button>
+     <input value={state.name} onChange={e =>setState({...state,name:e.target.value})} />
     </div>
   );
 }
